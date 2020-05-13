211869995513d8d3463c2550148ae595de866eaf
Y-HL <64395837+Y-HL@users.noreply.github.com>
2020-05-14 00:54:48 +0800

    Update 104_job.py

--- 104_job.py
+++ 104_job.py
@@ -6,18 +6,27 @@ import json
 headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
 
+page = 1
 url = 'https://www.104.com.tw/jobs/search/?'
-my_params = {'keyword': '大數據','order':'1','asc':'0','page':1,'mode':'s'}
+my_params = {'keyword': '大數據',\
+             'order':'15','asc':'0','page':page,'mode':'s'}
 
 res = requests.get(url,params=my_params ,headers=headers)
 soup = BeautifulSoup(res.text, 'html.parser')
 
-passw = soup.find('div', {"id":"js-job-content"}).find_all('a')[0]['href'].split('job/')[1].split('?')[0]
+i = 0
+passw = soup.find('div', {"id":"js-job-content"}).find_all('a')[i]['href'][21:26]
 
-i=0
-headers.update({'Referer':'https://www.104.com.tw/job/{}?jobsource=jolist_a_relevance'.format(passw)})
-url2 = 'https://www.104.com.tw/job/ajax/content/{}'.format(passw)
-resp=requests.get(url2,headers=headers)
+headers.update({'Referer':'https://www.104.com.tw/job/' + passw + '?jobsource=jolist_a_relevance'})
+url2 = 'https://www.104.com.tw/job/ajax/content/' + passw
+res2 = requests.get(url2,headers=headers)
+jdata = json.loads(res2.text)
 
-jdata=json.loads(resp.text)
-print(jdata)
+jobName = jdata['data']['header']['jobName'].replace('"', '_')
+comName = jdata['data']['header']['custName']
+appearDate = jdata['data']['header']['appearDate']
+salary = jdata['data']['jobDetail']['salary']
+salaryMin = jdata['data']['jobDetail']['salaryMin']
+salaryMax = jdata['data']['jobDetail']['salaryMax']
+industry = jdata['data']['industry']
+hrName = jdata['data']['contact']['hrName']
