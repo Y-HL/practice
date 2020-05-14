3b14ad3b8818199cf6e55cfef0ac9f966415a20e
Y-HL <64395837+Y-HL@users.noreply.github.com>
2020-05-15 07:37:02 +0800

    Update 104_job.py

--- 104_job.py
+++ 104_job.py
@@ -7,11 +7,9 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
 
 columns = ['jobName','comName','appearDate','salary','salaryMin','salaryMax','industry','hrName']
-data=[]
-jdata=[]
 url = 'https://www.104.com.tw/jobs/search/?'
-my_params = {'keyword': '大數據',\  # set the search keyword
-            'order':'15','asc':'0','mode':'s'}
+my_params = {'keyword': '大數據',\
+            'order':'15','asc':'0','mode':'s'} # set the search keyword
 
 for page in range(2):  # set total download pages
     res = requests.get(url,params=my_params.update({'page':page+1}) ,headers=headers)
@@ -36,8 +34,8 @@ for page in range(2):  # set total download pages
         data.append(content_data)
         print(content_data)
 
-df = pd.DataFrame(data = data, columns = columns)
-df.to_csv('./104_work.csv', index = 0, encoding = 'utf-8-sig')
+df = pd.DataFrame(data=data, columns=columns)
+df.to_csv('./104_work.csv', index=0, encoding='utf-8-sig')
 
 # Thanks for jimmyyang886 -- https://github.com/jimmyyang886/Club-TEB101-homework104/blob/master/hw_job104_json2pandas.ipynb
 #            Yuting(Tiffany)       
